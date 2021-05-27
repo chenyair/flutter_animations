e94f6115d6d9c008c631ca8f3e34266a2b59e0db
Yair Chen <u8706749@bsmch.net>
2021-05-27 11:06:42 +0300

    add animated circle

--- animatons/lib/circle_page_transition/ui/views/home_view.dart
+++ animatons/lib/circle_page_transition/ui/views/home_view.dart
@@ -1,6 +1,34 @@
-import 'package:animatons/circle_page_transition/core/viewmodels/home_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import 'package:animatons/circle_page_transition/core/viewmodels/home_model.dart';
+import 'package:animatons/circle_page_transition/ui/shared/globals.dart';
+
+class AnimatedCircle extends AnimatedWidget {
+  final Tween<double> tween;
+  final Animation<double> animation;
+  final double flip;
+  final Color color;
+  final Animation<double>? horizontalAnimation;
+  final Tween<double>? horizontalTween;
+
+  AnimatedCircle({
+    Key? key,
+    required this.tween,
+    required this.animation,
+    required this.flip,
+    required this.color,
+    this.horizontalAnimation,
+    this.horizontalTween,
+  })  : assert(flip == 1 || flip == -1),
+        super(key: key, listenable: animation);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
 
 class HomeView extends StatefulWidget {
   @override
@@ -9,9 +37,9 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> with TickerProviderStateMixin {
   late AnimationController animationController;
-  late Animation startAnimation;
-  late Animation endAnimation;
-  late Animation horizontalAnimation;
+  late Animation<double> startAnimation;
+  late Animation<double> endAnimation;
+  late Animation<double> horizontalAnimation;
   late PageController pageController;
 
   @override
@@ -63,6 +91,69 @@ class _HomeViewState extends State<HomeView> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final model = Provider.of<HomeModel>(context);
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      backgroundColor:
+          model.isHalfWay ? model.foreGroundColor : model.backGroundColor,
+      body: Stack(
+        children: [
+          Container(
+            color:
+                model.isHalfWay ? model.foreGroundColor : model.backGroundColor,
+            width: screenWidth / 2.0 - Global.radius / 2.0,
+            height: double.infinity,
+          ),
+          Transform(
+            transform: Matrix4.identity()
+              ..translate(
+                screenWidth / 2 - Global.radius / 2.0,
+                screenHeight - Global.radius - Global.bottomPadding,
+              ),
+            child: GestureDetector(
+              onTap: () {
+                if (animationController.status != AnimationStatus.forward) {
+                  model.isToggled = !model.isToggled;
+                  model.index++;
+                  if (model.index > 3) {
+                    model.index = 0;
+                  }
+                  pageController.animateToPage(
+                    model.index,
+                    duration: const Duration(milliseconds: 500),
+                    curve: Curves.easeInOutQuad,
+                  );
+                  animationController.forward();
+                }
+              },
+              child: Stack(
+                children: [
+                  AnimatedCircle(
+                    animation: startAnimation,
+                    color: model.foreGroundColor,
+                    flip: 1.0,
+                    tween: Tween<double>(
+                      begin: 1.0,
+                      end: Global.radius,
+                    ),
+                  ),
+                  AnimatedCircle(
+                    animation: endAnimation,
+                    color: model.backGroundColor,
+                    flip: -1.0,
+                    horizontalTween:
+                        Tween<double>(begin: 0, end: -Global.radius),
+                    horizontalAnimation: horizontalAnimation,
+                    tween: Tween<double>(begin: Global.radius, end: 1.0),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
